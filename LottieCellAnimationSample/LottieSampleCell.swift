0ee7148bd943b967e636513ff832e9c4edd472c7
Morita Jun <morita@onespal.co.jp>
2017-12-31 17:11:27 +0900

    first commit

---
+++ LottieCellAnimationSample/LottieSampleCell.swift
@@ -0,0 +1,24 @@
+//
+//  LottieSampleCell.swift
+//  LottieCellAnimationSample
+//
+//  Created by Morita Jun on 2017/12/30.
+//  Copyright © 2017年 kamuiproject. All rights reserved.
+//
+
+import UIKit
+
+class LottieSampleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LottieCellAnimationSample/Todo.swift
@@ -0,0 +1,9 @@
+//
+//  Todo.swift
+//  LottieCellAnimationSample
+//
+//  Created by Morita Jun on 2017/12/30.
+//  Copyright © 2017年 kamuiproject. All rights reserved.
+//
+
+import Foundation
